1f94ce8819d3fcea68f75e54362e7dbf53e64a16
Leroy Morales <leroy@nioyatech.com>
2022-10-08 21:19:29 +0300

    Updates

---
+++ precache-manifest.726fd979df735993a289a3b1d4e89ce3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cc8631799acc05a01b9",
+    "url": "/resume/static/css/main.196591f8.chunk.css"
+  },
+  {
+    "revision": "8cc8631799acc05a01b9",
+    "url": "/resume/static/js/main.8cc86317.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/resume/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "0bb8937a63640977a64426d84a92d03a",
+    "url": "/resume/index.html"
+  }
+];
