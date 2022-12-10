682b4974dd55dc3c9b2cc5bac5bf8625ae640078
Leroy Morales <leroy@nioyatech.com>
2022-12-10 21:38:56 +0300

    Updates

--- precache-manifest.9ab8868603031c94fa31e4005fd12b67.js
+++ precache-manifest.9ab8868603031c94fa31e4005fd12b67.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e0d460e05c7c7478f997",
+    "revision": "10bfb840069423b6a817",
     "url": "/resume/static/css/main.196591f8.chunk.css"
   },
   {
-    "revision": "e0d460e05c7c7478f997",
-    "url": "/resume/static/js/main.e0d460e0.chunk.js"
+    "revision": "10bfb840069423b6a817",
+    "url": "/resume/static/js/main.10bfb840.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/resume/static/js/runtime~main.18bea22a.js"
   },
   {
-    "revision": "e5993041b4b23837939f200ca4a711f5",
+    "revision": "0d8185f3a8e46bebe121e84ae9e82646",
     "url": "/resume/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.ba4a1a3efcad96342ea111ad254c333b.js"
+  "/resume/precache-manifest.9ab8868603031c94fa31e4005fd12b67.js"
 );
 
 workbox.clientsClaim();
