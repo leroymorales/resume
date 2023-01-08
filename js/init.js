0e7b8e628add8d987acf4a4815dd8c53817403a3
Leroy Morales <leroy@nioyatech.com>
2023-01-09 00:26:06 +0300

    Updates

--- js/init.js
+++ js/init.js
@@ -105,12 +105,12 @@ jQuery(document).ready(function ($) {
    $('.item-wrap a').magnificPopup({
       items: {
          src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
-         title: 'Peter & Paul fortress in SPB'
+         title: 'Peter & Paul fortress in SPB',
+         fixedContentPos: false,
+         removalDelay: 200,
+         showCloseBtn: false,
       },
       type: 'image',
-      fixedContentPos: false,
-      removalDelay: 200,
-      showCloseBtn: false,
       // mainClass: 'mfp-fade'
 
    });
